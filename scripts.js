00e7c3de0b0ac9ae64c08fbeaae92d8ba40aa595
Maciej Rut <wirtmaciej@gmail.com>
2017-08-23 12:02:27 +0200

    scripts modified

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ $(function() {
     function randomString() {
 	    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
 	    var str = '';
+	    var i = 0;
 	    for (i = 0; i < 10; i++) {
 	        str += chars[Math.floor(Math.random() * chars.length)];
 	    }
